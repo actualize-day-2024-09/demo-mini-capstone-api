a82195676cfbe1d0dcc00d1f2d79816b06be492a
Peter Jang <peter.x.jang@gmail.com>
2024-11-06 14:05:56 -0600

    Add routes for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     @products = Product.all
     render template: "products/index"
   end
+
+  def first_product
+    @product = Product.find_by(id: 1)
+    render template: "products/show"
+  end
+
+  def second_product
+    @product = Product.find_by(id: 2)
+    render template: "products/show"
+  end
+
+  def third_product
+    @product = Product.find_by(id: 3)
+    render template: "products/show"
+  end
 end
