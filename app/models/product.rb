4f8f077e73d041045ac83a318bd0ca17c4ed5cca
Peter Jang <peter.x.jang@gmail.com>
2024-11-08 14:57:20 -0600

    Add model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  TAX_RATE = 0.09
+
+  def is_discounted?
+    price <= 10
+  end
+
+  def tax
+    price * TAX_RATE
+  end
+
+  def total
+    price + tax
+  end
 end
