394ab26dc5928cb9fd6094e3a79479a0293da129
Peter Jang <peter.x.jang@gmail.com>
2024-11-11 14:14:51 -0600

    Add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 1..5000 }
+
   TAX_RATE = 0.09
 
   def is_discounted?
